330c64041772fbb9f1cee56eda7e795703b8b498
Unknown <dajka1989@gmail.com>
2020-01-06 15:10:10 +0100

    !F Freeze

--- Project-Cut-Your_pictures/Assets/Scripts/Cutter.cs
+++ Project-Cut-Your_pictures/Assets/Scripts/Cutter.cs
@@ -1,27 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cutter : MonoBehaviour
+public class Cutter : MonoBehaviourSingleton<Cutter>
 {
+	public delegate void FreezeCountChangeHandler(int newValue);
+	public event FreezeCountChangeHandler FreezeCountChanged;
+
 	public float speed = 0.01f;
 	public float rotationAngle = 1f;
+	public float FreezeDuration = 2f;
 
 	Vector2 direction = Vector2.right;
-	bool rotating;
 	int rotateDirection;
+	bool rotating;
+
+	bool isFreezed;
+	public int FreezeCount { get; private set; } = 10;
+
+	Bounds cuttingArea;
+
+
+	private void Start()
+	{
+		cuttingArea = GameObject.Find("CuttingTable").GetComponent<BoxCollider2D>().bounds;
+		FreezeCountChanged += OnFreezeCountChanged;
+	}
 
 	void Update()
     {
-		Vector3 translation = transform.up * speed;
-
-		transform.Translate(Vector3.up * speed);
+		if(!isFreezed)
+			Move();
 
 		if (rotating)
 			Rotate();
 	}
 
-	public void Rotate()
+	private void Move()
+	{
+		transform.Translate(Vector3.up * speed);
+
+		LimitInCuttingArea();
+	}
+
+	private void LimitInCuttingArea()
+	{
+		float x = Mathf.Clamp(transform.position.x, cuttingArea.center.x - cuttingArea.extents.x, cuttingArea.center.x + cuttingArea.extents.x);
+		float y = Mathf.Clamp(transform.position.y, cuttingArea.center.y - cuttingArea.extents.y, cuttingArea.center.y + cuttingArea.extents.y);
+
+		transform.position = new Vector2(x, y);
+	}
+
+	void Rotate()
 	{
 		transform.Rotate(0, 0, transform.rotation.eulerAngles.x + rotationAngle * rotateDirection);
 	}
@@ -35,5 +66,29 @@ public class Cutter : MonoBehaviour
 	public void EndRotation()
 	{
 		rotating = false;
+	}
+
+	public void Freeze()
+	{
+		if (isFreezed || FreezeCount == 0)
+			return;
+
+		isFreezed = true;
+
+		FreezeCount--;
+		FreezeCountChanged(FreezeCount);
+
+		StartCoroutine(UnFreezeAfterTimeOutRoutine());
+	}
+
+	private IEnumerator UnFreezeAfterTimeOutRoutine()
+	{
+		yield return new WaitForSeconds(FreezeDuration);
+		isFreezed = false;
+	}
+
+	private void OnFreezeCountChanged(int newValue)
+	{
+		//dummy
 	}
 }
---
+++ Project-Cut-Your_pictures/Assets/Scripts/UIHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIHandler : MonoBehaviour
+{
+	TextMeshProUGUI freezeCountLbl;
+
+	void Start()
+    {
+		freezeCountLbl = transform.Find("Freeze/FreezeCountLbl").GetComponent<TextMeshProUGUI>();
+
+		Cutter.instance.FreezeCountChanged += OnFreezeCountChanged;
+	}
+
+    void Update()
+    {
+        
+    }
+
+	void OnFreezeCountChanged(int newValue)
+	{
+		freezeCountLbl.text = newValue + "x";
+	}
+}
