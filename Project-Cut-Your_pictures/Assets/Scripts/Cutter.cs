a3ffdbdd600074d0006875a6faaf04b27175692c
Unknown <dajka1989@gmail.com>
2020-01-06 14:08:42 +0100

    !F Rotate cutter

---
+++ Project-Cut-Your_pictures/Assets/Scripts/Cutter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutter : MonoBehaviour
+{
+	public float speed = 0.01f;
+	public float rotationAngle = 1f;
+
+	Vector2 direction = Vector2.right;
+	bool rotating;
+	int rotateDirection;
+
+	void Update()
+    {
+		Vector3 translation = transform.up * speed;
+
+		transform.Translate(Vector3.up * speed);
+
+		if (rotating)
+			Rotate();
+	}
+
+	public void Rotate()
+	{
+		transform.Rotate(0, 0, transform.rotation.eulerAngles.x + rotationAngle * rotateDirection);
+	}
+
+	public void StartRotation(int rotateDirection)
+	{
+		this.rotateDirection = rotateDirection;
+		rotating = true;
+	}
+
+	public void EndRotation()
+	{
+		rotating = false;
+	}
+}
