06f4fb0e2bbc0c546d67e1279ce64515fe9027dc
Dajka Ferenc <dajka1989@gmail.com>
2020-01-08 15:37:31 +0100

    !A Zooming in pictures of Main Menu

---
+++ Project-Cut-Your_pictures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviourSingleton<CameraController>
+{
+	const float zoomedInCameraSize = 1.5f;
+	const float zoomedOutCameraSize = 5f;
+	
+	public float focusAnimTime = .5f;
+	public float zoomAnimTime = .5f;
+
+	new Camera camera;
+
+	bool IsZoomedIn
+	{
+		get => camera.orthographicSize - zoomedInCameraSize < Mathf.Epsilon;
+	}
+
+	private void Start()
+	{
+		camera = GetComponent<Camera>();
+	}
+
+	public void FocusImage(Vector2 imageCenter)
+	{
+		StartCoroutine(FocusImageRoutine(imageCenter));
+	}
+
+	IEnumerator FocusImageRoutine(Vector2 imageCenter)
+	{
+		if (IsZoomedIn)
+			yield return ZoomRoutine(ZoomType.Out);
+
+		yield return PositionOnImageCenterRoutine(imageCenter);
+		yield return ZoomRoutine(ZoomType.In);
+	}
+
+	IEnumerator PositionOnImageCenterRoutine(Vector2 imageCenter)
+	{
+		float timePassed = 0;
+		Vector3 orignalPositon = transform.position;
+
+		while(timePassed < focusAnimTime)
+		{
+			float t = timePassed / focusAnimTime;
+
+			Vector3 currentPosition = Vector2.Lerp(orignalPositon, imageCenter, t);
+			currentPosition.z = orignalPositon.z;
+			transform.position = currentPosition;
+
+			timePassed += Time.deltaTime;
+			yield return null;
+		}
+
+		transform.position = new Vector3(imageCenter.x, imageCenter.y, orignalPositon.z);
+	}
+
+	IEnumerator ZoomRoutine(ZoomType zoomType)
+	{
+		float timePassed = 0;
+
+		while (timePassed < zoomAnimTime)
+		{
+			float t = timePassed / zoomAnimTime;
+
+			if (zoomType == ZoomType.Out)
+				t = 1 - t;
+
+			camera.orthographicSize = Mathf.Lerp(zoomedOutCameraSize, zoomedInCameraSize, t);
+
+			timePassed += Time.deltaTime;
+			yield return null;
+		}
+
+		switch (zoomType)
+		{
+			case ZoomType.In:
+				camera.orthographicSize = zoomedInCameraSize;
+				break;
+			case ZoomType.Out:
+				camera.orthographicSize = zoomedOutCameraSize;
+				break;
+			default:
+				break;
+		}
+	}
+
+	enum ZoomType
+	{
+		In,
+		Out
+	}
+}
---
+++ Project-Cut-Your_pictures/Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuHandler : MonoBehaviour
+{
+	public GameObject[] pictures;
+	int currentPictureIndex = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			CameraController.instance.FocusImage(pictures[currentPictureIndex].transform.position);
+			currentPictureIndex++;
+		}
+    }
+}
