a88133dbd86917e0b7cdf0bd7ac94ba95101caed
Unknown <dajka1989@gmail.com>
2020-01-16 09:10:28 +0100

    !F You can cut texture with finger

--- Project-3D/Assets/Scripts/Quad.cs
+++ Project-3D/Assets/Scripts/Quad.cs
@@ -4,29 +4,50 @@ using UnityEngine;
 
 public class Quad : MonoBehaviour
 {
+	public int halfCutSize = 3;
+
 	Texture2D currentTexture;
 	readonly Color Alpha = new Color(0, 0, 0, 0);
+	Vector2Int textureSize;
+	
 
 	void Start()
     {
 		Texture2D loadedTexture = Resources.Load<Texture2D>("texture");
 		currentTexture = new Texture2D(loadedTexture.width, loadedTexture.height);
 		currentTexture.SetPixels(loadedTexture.GetPixels());
+		currentTexture.Apply();
+
+		textureSize = new Vector2Int(currentTexture.width, currentTexture.height);
+
 		GetComponent<Renderer>().material.mainTexture = currentTexture;
+
 	}
 
-    void Update()
+	void Update()
     {
-		for (int i = 0; i < 1000; ++i)
+		if (Input.GetMouseButton(0))
 		{
-			Vector2Int randomPos = new Vector2Int(Random.Range(0, currentTexture.width), Random.Range(0, currentTexture.height));
-			Cut(randomPos);
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit, 100.0f))
+			{
+				Vector2 cutPosition = hit.textureCoord * textureSize;
+				Cut(cutPosition.ToVector2Int());
+			}
 		}
-		currentTexture.Apply();
 	}
+
 
 	void Cut(Vector2Int cutPosition)
 	{
-		currentTexture.SetPixel(cutPosition.x, cutPosition.y, Alpha);
+		for (int i = -halfCutSize; i < halfCutSize; ++i)
+		{
+			for (int j = -halfCutSize; j < halfCutSize; ++j)
+			{
+				currentTexture.SetPixel(cutPosition.x + i, cutPosition.y + j, Alpha);
+			}
+		}
+		currentTexture.Apply();
 	}
 }
---
+++ Project-3D/Assets/Scripts/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils
+{
+	public static Vector2Int ToVector2Int(this Vector2 v)
+	{
+		return new Vector2Int((int)v.x, (int)v.y);
+	}
+
+}
